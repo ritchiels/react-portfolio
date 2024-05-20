30ca313031b908e4f60c8bb62759b1acc12dcffd
Ritchie Simmons <ritchiels.alt@gmail.com>
2024-05-20 04:05:11 -0400

    updated projects, fixed responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import DevBotPic from '../img/dev-bot-screen.png'
-import PromptopiaPic from '../img/promptopia-screen.png'
+import BrainwavePic from '../img/brainwave-screen.png'
 import SumUpPic from '../img/sum-up-screen.png'
 import Vite from '../img/vite.svg'
 import Github from '../img/github.svg'
@@ -22,6 +22,12 @@ const Projects = () => {
                         <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
                             <img src={Vite} className="vite-pic" />
                         </a>
+                        <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer" htmlFor="html">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" className="project-stack-icons" />
+                        </a>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noreferrer" htmlFor="css">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className="project-stack-icons" />
+                        </a>
                         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer" htmlFor="javascript">
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className="project-stack-icons" />
                         </a>
@@ -29,7 +35,7 @@ const Projects = () => {
                 </div>
                 <div className="dev-bot-text flex flex-col items-center justify-center md:pl-10">
                     <h2 className="text-lapiz-lazuli text-xl font-montserrat font-semibold">Dev Bot</h2>
-                    <p className="text-gray-600 text-sm font-comfortaa w-60 font-medium text-center">a chat bot made with JavaScript and Vite using OpenAI's api to utilize their gpt-3.5-turbo model</p>
+                    <p className="text-gray-600 text-sm font-comfortaa w-80 font-medium text-center">your new coding buddy, a chatbot made with JavaScript using OpenAI's api to utilize their gpt-3.5-turbo model</p>
                     <div className="flex flex-row space-x-12 justify-center items-center mt-2">
                         <a href="https://github.com/ritchiels/vite-js-devbot" target="_blank" rel="noreferrer" className="flex flex-col items-center no-underline">
                             <img src={Github} className="code-demo animate-bounce" />
@@ -42,36 +48,35 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-            {/* promptopia */}
-            <div className="promptopia flex flex-row mb-20">
-                <div className="promptopia-text flex flex-col items-center justify-center md:pr-10">
-                    <h2 className="text-lapiz-lazuli text-xl font-montserrat font-semibold mb-0">Promptopia</h2>
-                    <p className="text-gray-600 text-sm m-0">(W.I.P.)</p>
-                    <p className="text-gray-600 text-sm font-comfortaa w-80 font-medium text-center">a forum for sharing and discovering AI prompts, complete with O-Auth sign-in, made using Next.js, Tailwind and MongoDB</p>
+            {/* brainwave */}
+            <div className="brainwave flex flex-row mb-20">
+                <div className="brainwave-text flex flex-col items-center justify-center md:pr-10">
+                    <h2 className="text-lapiz-lazuli text-xl font-montserrat font-semibold">Brainwave</h2>
+                    <p className="text-gray-600 text-sm font-comfortaa w-80 font-medium text-center">a product page for an AI chatbot service, made with React and Tailwind, featuring a fully responsive, sleek design and several reusable components.</p>
                     <div className="flex flex-row space-x-12 justify-center items-center mt-2">
-                        <a href="https://github.com/ritchiels/nextjs-promptopia" target="_blank" rel="noreferrer" className="flex flex-col items-center no-underline">
+                        <a href="https://github.com/ritchiels/brainwave" target="_blank" rel="noreferrer" className="flex flex-col items-center no-underline">
                             <img src={Github} className="code-demo animate-bounce" />
                             <p className={`text-gray-600 text-sm font-comfortaa ${theme === 'dark' && 'animate-bounce'}`}>Code</p>
                         </a>
-                        <a href="https://nextjs-promptopia-kappa.vercel.app/" target="_blank" rel="noreferrer" className="flex flex-col items-center no-underline">
+                        <a href="https://brainwave-gilt-one.vercel.app/" target="_blank" rel="noreferrer" className="flex flex-col items-center no-underline">
                             <img src={Vercel} className="code-demo animate-bounce" />
                             <p className={`text-gray-600 text-sm font-comfortaa ${theme === 'dark' && 'animate-bounce'}`}>Demo</p>
                         </a>
                     </div>
                 </div>
                 <div className="flex flex-col">
-                    <a href="https://nextjs-promptopia-kappa.vercel.app/" target="_blank" rel="noreferrer">
-                        <img src={PromptopiaPic} className="project-images rounded-lg hover:animate-pulse" />
+                    <a href="https://brainwave-gilt-one.vercel.app/" target="_blank" rel="noreferrer">
+                        <img src={BrainwavePic} className="project-images rounded-lg hover:animate-pulse" />
                     </a>
                     <div className="flex flex-row space-x-6 m-auto mt-3">
-                        <a href="https://nextjs.org/docs" target="_blank" rel="noreferrer" htmlFor="nextjs">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" className="project-stack-icons" />
+                        <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
+                            <img src={Vite} className="vite-pic" />
+                        </a>
+                        <a href="https://react.dev/" target="_blank" rel="noreferrer" htmlFor="react">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className="project-stack-icons" />
                         </a>
                         <a href="https://tailwindcss.com/docs/installation" target="_blank" rel="noreferrer" htmlFor="tailwind">
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" className="project-stack-icons" />
-                        </a>
-                        <a href="https://www.mongodb.com/docs/" target="_blank" rel="noreferrer" htmlFor="mongodb">
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" className="project-stack-icons" />
                         </a>
                     </div>
                 </div>
